afdb64c7186f6df08e0f37d8d6aec6e5f2e491e7
Ye Jia <ye.jia.alice@gmail.com>
2017-03-20 23:43:58 +0800

    store是命名空间

--- first_redux.js
+++ first_redux.js
@@ -12,9 +12,12 @@ const reducer = function (state, action) {
   }
 }
 // 初始化设置
+// subscribe并不属于，而是属于store的命名空间
 const store = Redux.createStore(reducer, { name: 'alice' });
+var subscribe = store.subscribe;
+var { subscribe, dispatch, getState } = store;
 store.subscribe(() => console.log(store.getState()));
-
+/*
 function callAction() {
   var promiseAction = new Promise(function (resolve, reject) {
     setTimeout(function () {
@@ -30,10 +33,41 @@ function callAction() {
   promiseAction.then(function (action) {
     store.dispatch(action);
   });
-}
+}*/
 const action = {
   type: 'changeName',
   name: 'baby'
 }
-// store.dispatch(action);
-callAction();
+store.dispatch(action);
+// callAction();
+
+function User() {
+  this.name = 'alice';
+  this.changeName = function (name) {
+    this.name = name;
+  }
+}
+
+/*命名空间，内部没有this*/
+function createStoreCS(){
+  let myname = 'alice';
+  function dispatch(){
+    console.log(myname);
+  }
+  return {
+    dispatch: dispatch
+  }
+}
+
+var storeCS = createStoreCS();
+storeCS.dispatch();
+var dispatchCS = storeCS.dispatch;
+dispatchCS();
+
+var u = new User();
+u.changeName('yejia');
+console.log(u.name);
+
+// TypeError: u.changename is not a function
+// var changeName = u.changeName;
+// changeName('baby');
