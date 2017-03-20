7fc1a89b1561fce364e6630d974fbde5b2587ccb
Ye Jia <ye.jia.alice@gmail.com>
2017-03-20 23:27:45 +0800

    JSON.parse新建对象

--- first_redux.js
+++ first_redux.js
@@ -1,17 +1,23 @@
 const Redux = require('redux');
+// reducer是一个纯函数
 const reducer = function (state, action) {
-  if (action.type === 'cangeName') {
-    return Object.assign({}, state, {name: action.name});
+  if (action.type === 'changeName') {
+    console.log('changeName');
+    return Object.assign({}, state, { name: action.name });
+    // 或者通过JSON.parse新建对象，不能直接更改state
+    // var newState = JSON.parse(JSON.stringify(state));
+    // return Object.assign({}, state, newState);
   } else {
-    return state;
+    return state; // 没有匹配的action.type就返回原来的state
   }
 }
-const store = Redux.createStore(reducer, {name: 'alice'});
+// 初始化设置
+const store = Redux.createStore(reducer, { name: 'alice' });
 store.subscribe(() => console.log(store.getState()));
 
-function callAction () {
-  var promiseAction = new Promise(function(resolve, reject){
-    setTimeout(function (){
+function callAction() {
+  var promiseAction = new Promise(function (resolve, reject) {
+    setTimeout(function () {
       var action = {
         type: 'test',
         data: {
@@ -19,15 +25,15 @@ function callAction () {
         }
       };
       resolve(action);
-    }, 1000);  
+    }, 1000);
   });
-  promiseAction.then(function(action){
+  promiseAction.then(function (action) {
     store.dispatch(action);
   });
 }
-// const action = {
-//   type: 'changeName',
-//   name: 'baby'
-// }
+const action = {
+  type: 'changeName',
+  name: 'baby'
+}
 // store.dispatch(action);
 callAction();
